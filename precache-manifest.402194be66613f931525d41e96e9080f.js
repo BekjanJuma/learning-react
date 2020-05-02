5f770a67358eb9ccfca9643c12a32a8c59d9dd5a
bekjan.jumabaev <bekjan.jumabaev@financesoft.kg>
2020-05-03 02:21:05 +0600

    Updates

---
+++ precache-manifest.402194be66613f931525d41e96e9080f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5344e5d7b660c35ed74aeb0a2cfb9867",
+    "url": "/learning-react/monsters-profile/index.html"
+  },
+  {
+    "revision": "3b4f5d1629725abafb58",
+    "url": "/learning-react/monsters-profile/static/css/main.ce4fad8a.chunk.css"
+  },
+  {
+    "revision": "46d0764b2a7824f28bf6",
+    "url": "/learning-react/monsters-profile/static/js/2.7a39425b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learning-react/monsters-profile/static/js/2.7a39425b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4f5d1629725abafb58",
+    "url": "/learning-react/monsters-profile/static/js/main.9391c6a1.chunk.js"
+  },
+  {
+    "revision": "2fdb7f614734ef5a4886",
+    "url": "/learning-react/monsters-profile/static/js/runtime-main.0c2e6076.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/learning-react/precache-manifest.5688e52500c34e97baadc21e4124513d.js"
+  "/learning-react/monsters-profile/precache-manifest.402194be66613f931525d41e96e9080f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/learning-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/learning-react/monsters-profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
